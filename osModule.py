9519a047448add55959887c5cfd8052130c44f49
brettrick4 <58719370+brettrick4@users.noreply.github.com>
2020-03-02 13:07:17 -0800

    Re-submition of drill
    I added some comments to hopefully make more sense out of what  was submitted a moment ago.

--- osModule.py
+++ osModule.py
@@ -1,12 +1,14 @@
 import os
 items = os.listdir("C:\sqlite\db\pythonsqlite.db")
 print(items)
+# This lists the contents of the directory in pythonsqlite.db
 
 newlist = []
 for names in items:
     if names.endswith(".txt"):
         newlist.append(names)
 print(newlist)
+# This lists the text files in the directory that end with ".txt"
 
 fName = 'Hello.txt'
 fPath = 'C:\\sqlite\\db\\pythonsqlite.db'
@@ -14,7 +16,7 @@ fPath = 'C:\\sqlite\\db\\pythonsqlite.db'
 
 abPath = os.path.join(fPath, fName)
 print(abPath)
-
+# This joins the file name to the file path
 
 import os
 import sys
@@ -33,10 +35,13 @@ local_time = time.ctime(modification_time)
 print("Last modification time(Local time):", local_time)
 
 print(os.getcwd())
+'''This tells the time in microseconds since the file path was modified, it also
+lists the time local time the file path was midified'''
 
 with open("ourModule.py", 'r') as f:
     data = f.read()
     print(data)
     f.close()
-
+'''This reads the information from the file "ourModule.py" and has been included
+in this GitHun submittion just for funsies'''
 
